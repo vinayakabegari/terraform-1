23bd5f1dde89d5e36cc764fbf8e4fa1f5f78bbc1
rajulucky812 <97225776+rajulucky812@users.noreply.github.com>
2022-06-01 09:56:20 +0530

    Update main.tf

--- 01.terraform-sample-ec2-userdata/main.tf
+++ 01.terraform-sample-ec2-userdata/main.tf
@@ -8,16 +8,25 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  ami                    = "ami-04505e74c0741db8d"
+  ami                    = "ami-0022f774911c1d690"
   instance_type          = "t2.micro"
-  key_name               = "vpectesting"
+  key_name               = "terraform"
   monitoring             = true
-  vpc_security_group_ids = ["sg-026e6de718633f70f"]
-  subnet_id              = "subnet-0ac4be71aa723b6f6"
+  vpc_security_group_ids = ["sg-020d448c9c2494131"]
+  subnet_id              = "subnet-09944b8728c9c3b55"
   
   tags = {
     Name = "terraform-instance"
     Terraform   = "true"
     Environment = "dev"
+  }
+}
+
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "my-terraform-state-s3test"
+    region = "us-east-1"
+    key = "terraform-state/terraform.tfstate"
   }
 }
